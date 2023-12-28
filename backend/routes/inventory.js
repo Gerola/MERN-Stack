0633f9d1e6b4279420aee7f066bdf8e3bcd09fcf
Matthew Gerola <97924327+Gerola@users.noreply.github.com>
2023-12-28 12:20:30 -0800

    Add files via upload

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,1 +1,22 @@
+const express = require('express')
+const {
+    createItem,
+    getItem,
+    getItems,
+    deleteItem,
+    updateItem
+} = require('../controllers/inventoryController')
 
+const router = express.Router()
+
+router.get('/', getItems)
+
+router.get('/:id', getItem)
+
+router.post('/',createItem)
+
+router.patch('/:id',updateItem)
+
+router.delete('/:id',deleteItem)
+
+module.exports = router
