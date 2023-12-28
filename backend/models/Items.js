99bc4beb1a77c5fb298096dd0dcc4104952c28ee
Matthew Gerola <97924327+Gerola@users.noreply.github.com>
2023-12-28 12:19:48 -0800

    Add files via upload

--- backend/models/Items.js
+++ backend/models/Items.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose')
 
+const Schema = mongoose.Schema
+
+const ItemsSchema = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    category: {
+        type: String,
+        require: true
+    },
+    quantity: {
+        type: Number,
+        require: true
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('Items',ItemsSchema)
