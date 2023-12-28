3c68ccfca90bf6f957188303ef758fdc1ec21823
Matthew Gerola <97924327+Gerola@users.noreply.github.com>
2023-12-28 12:25:35 -0800

    Add files via upload

--- frontend/src/hooks/useItemscontext.js
+++ frontend/src/hooks/useItemscontext.js
@@ -1,1 +1,14 @@
+import { ItemContext } from "../context/itemcontext";
+import { useContext } from "react";
 
+export const useItemContext = () =>
+{
+    const context = useContext(ItemContext)
+    
+    if (!context)
+    {
+        throw Error("Use Item context")
+    }
+    
+    return context
+}
